b5fb045e0ab7527b7b838776b18bde746bfcb5fb
Intermezzo <godomega@gmail.com>
2011-09-29 15:03:56 +0200

    Added minimal functional server

---
+++ TrafficController/TrafficController/Controller/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficController
+{
+    class Controller
+    {
+        //todo: start the new server, and the controller on a new thread
+        void Mainloop()
+        {
+
+        }
+    }
+}
